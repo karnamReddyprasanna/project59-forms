0bfb2798438448d6378f6c78c68473fdc35330b3
karnamReddyprasanna <prasanareddy43@gmail.com>
2023-04-13 10:17:02 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def hai(request):
+    if request.method=='POST':
+        name=request.POST['un']
+        pw=request.POST['pw']
+        print(name)
+        print(pw)
+        return HttpResponse('data will be submitted')
+    return render(request,'hai.html')
